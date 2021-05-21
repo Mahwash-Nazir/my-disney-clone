e3e53cf8e2958379752dbb87a3c3f98125e4d535
Mahi <mahwash.nazir@gmail.com>
2021-05-21 13:36:55 +0530

    Disney clone

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Counter } from './features/counter/Counter'
+import './App.css'
+import Header from './components/Header'
+import Home from './components/Home'
+import DetailsPage from './components/DetailsPage'
+import Login from './components/Login'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/detail/:id'>
+            <DetailsPage />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
+}
+
+export default App
